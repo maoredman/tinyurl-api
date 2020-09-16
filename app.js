75fe8cd53ab710bb6583baff020964c1360ab9a1
maoredman <ccm29cam@gmail.com>
2020-09-16 22:05:48 +0800

    refactor code

--- app.js
+++ app.js
@@ -14,20 +14,17 @@ app.use(router.routes());
 
 async function postUrl(ctx) {
     const url = 'https://tinyurl.com/api-create.php?url=' + ctx.request.body.url;
-    let status;
-    const tinyUrl = await axios.get(url)
-        .then(function (response) {
-        	status = response.status;
-            return response.data;            
+    await axios.get(url)
+        .then(response => {
+            ctx.status = response.status;
+            if (ctx.status === 200) {
+                ctx.body = response.data;
+            }          
         })
-        .catch(function (error) {
-        	status = error.response.status;
-            // console.log(error);
+        .catch(error => {
+        	ctx.status = error.response.status;
         })
-    ctx.status = status;
-    if (status === 200) {
-    	ctx.body = tinyUrl;
-    }
+    
 }
 
 app.listen(PORT);
